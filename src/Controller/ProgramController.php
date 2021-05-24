cee132a39dd5f777c58dbe68f1d06420945370d3
Gersey Stelmach <gerseystelmach@gmail.com>
2021-05-24 13:41:34 +0200

    Quete 05 - Routing avance

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,17 +6,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+ /**
+    *  @Route("/programs", name="program_")
+*/
+
 class ProgramController extends AbstractController 
 {
+   
     /**
-    *  @Route("/programs/", name="program_index")
+    *  @Route("/", name="index")
     */
-
     public function index(): Response 
     {
-       
+    
+   
         return $this->render('program/index.html.twig', [
        'website' => 'Wild Séries',
     ]);
+    }
+
+    /**
+     * @Route("/{id}", name="show", methods={"GET"}, requirements={"id"="\d+"})
+     */
+
+    public function show(int $id): Response 
+    {
+           return $this->render('program/show.html.twig', ['id' => $id]);
     }
 }
